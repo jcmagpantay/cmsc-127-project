1bdebf1050b0dbb5f69c7d6966ac957d94f5aa0d
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-20 17:17:56 +0800

    feat: Initialize MariaDB instance

---
+++ main.py
@@ -0,0 +1,24 @@
+import mariadb
+import sys
+
+print("Connecting to database")
+
+
+# Connect to MariaDB Platform
+try:
+    db_user = input("MariaDB User: ")
+    db_password = input("Password: ")
+    db = input("Database to use: ")
+    conn = mariadb.connect(
+        user=db_user,
+        password=db_password,
+        host="127.0.0.1", # Connects to http://localhost:3306
+        port=3306,        # Assuming the MariaDB instance is there
+        database=db
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
