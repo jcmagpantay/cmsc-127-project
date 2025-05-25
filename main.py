d380aacb2283146c87fd7561fddf6d8470a0d498
a5ti <irmagmanlac@up.edu.ph>
2025-05-25 15:11:34 +0800

    feat: add create functions

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ try:
     conn = mariadb.connect(
         user=db_user,
         password=db_password,
-        host="127.0.0.1", # Connects to http://localhost:3306
-        port=3306,        # Assuming the MariaDB instance is there
-        database=db
+        host="127.0.0.1",  # Connects to http://localhost:3306
+        port=3306,  # Assuming the MariaDB instance is there
+        database=db,
     )
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
@@ -22,3 +22,103 @@ except mariadb.Error as e:
 
 # Get Cursor
 cur = conn.cursor()
+
+
+def createUser(name, gender, degree_program, password, access_level, username):
+    try:
+        cur.execute(
+            """INSERT INTO member (`name`, `gender`, `degree_program`,
+            `password`, `access_level`, `username`)
+            VALUES (?, ?, ?, ?, ?, ?)""",
+            (name, gender, degree_program, password, access_level, username),
+        )
+        conn.commit()
+        print(f"User {username} succesfully added")
+    except mariadb.Error as e:
+        print(f"Error occurred: {e}")
+
+
+def createOrg(organization_name, date_established):
+    try:
+        cur.execute(
+            """INSERT INTO organization (`organization_name`,
+        `date_established)VALUES (?, ?)""",
+            (organization_name, date_established),
+        )
+        conn.commit()
+        print(f"Organization {organization_name} succesfully added")
+    except mariadb.Error as e:
+        print(f"Error occurred: {e}")
+
+
+def createMember(
+    member_id, organization_id, batch, status, role, committee, acad_year, semester
+):
+    try:
+        cur.execute(
+            """INSERT INTO member_org VALUES
+            (?, ?, ?, ?, ?, ?, ?, ?)""",
+            (
+                member_id,
+                organization_id,
+                batch,
+                status,
+                role,
+                committee,
+                acad_year,
+                semester,
+            ),
+        )
+        conn.commit()
+        print("Member succesfully added!")
+    except mariadb.Error as e:
+        print(f"Error occurred: {e}")
+
+
+def createFinancialRecord(balance, academic_year, semester, organization_id):
+    try:
+        cur.execute(
+            """INSERT INTO financial_record
+        (`balance`, `academic_year`, `semester`, `organization_id`)
+        VALUES(?, ?, ?, ?)""",
+            (balance, academic_year, semester, organization_id),
+        )
+        conn.commit()
+        print("Record succesfully added!")
+    except mariadb.Error as e:
+        print(f"Error occurred: {e}")
+
+
+def createFee(
+    amount,
+    due_date,
+    date_issued,
+    fee_type,
+    payment_status,
+    pay_date,
+    description,
+    member_id,
+    record_id,
+):
+    try:
+        cur.execute(
+            """INSERT INTO fee(`amount`, `due_date`, `date_issued`,
+        `fee_type`,`payment_status`, `pay_date`,
+        `description`, `member_id`, `record_id`)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+            (
+                amount,
+                due_date,
+                date_issued,
+                fee_type,
+                payment_status,
+                pay_date,
+                description,
+                member_id,
+                record_id,
+            ),
+        )
+        conn.commit()
+        print("Fee succesfully created")
+    except mariadb.Error as e:
+        print(f"Error occurred: {e}")
