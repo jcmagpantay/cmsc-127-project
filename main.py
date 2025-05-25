c395ce42b4d3e615a88ad8a2c2de4d1ed1450c70
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-25 17:21:46 +0800

    feat: Add User class and separated views

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import mariadb
 import sys
 
-# Returns a boolean if login is a success or not
+class User:
+    def __init__(self, memberId: int, name: str, gender: str, degreeProgram: str, accessLevel: int, username: str):
+        self.memberId = memberId
+        self.name = name
+        self.gender = gender
+        self.degreeProgram = degreeProgram
+        self.accessLevel = accessLevel
+        self.username = username
+    
+    def getName(self):
+        return self.name
+    
+    def getAccessLevel(self):
+        return self.accessLevel
+    
+    def getUsername(self):
+        return self.username
+
+# Returns a User or None if login is a success or not
 # Fetches the associated member with the username
 def login(cur):
     username = input("Username: ")
@@ -17,17 +35,18 @@ def login(cur):
 
     if result == None:
         print("No user found with that username")
-        return False
+        return None
     
     matchedPassword = result[4]
     name = result[1]
 
     if matchedPassword != password:
         print("Invalid credentials!")
-        return False
+        return None
     
-    print("Logged in successfully! Welcome " + name + "!")
-    return True
+    user = User(result[0], name, result[2], result[3], result[5], result[6])
+    print("Logged in successfully! Welcome " + user.getName() + "!")
+    return user
     
 def getCreateMemberInput():
     return
@@ -48,17 +67,39 @@ def openReportsMenu():
     print("[3] ")
     return
 
-def menu(cur):
+# Shows the initial login, register, and exit menu
+def auth(cur):
 
+    choice = -1 # Menu choice
+    user = None # User instance
+                # -> that stores: name, username, accessLevel... etc
+
+    while choice != 0:
+        print("======= Welcome =======")
+        print("[1] Login")
+        print("[2] Register")
+        print("[0] Exit")
+        print("=======================")
+
+        choice = int(input("Choice: "))
+
+        match choice:
+            case 1:
+                user = login(cur)
+            case 2:
+                user = login(cur)
+            case 0:
+                break
+            
+        if user:
+            return user
+
+def adminMenu():
     choice = -1
 
     while choice != 0:
-        success = login(cur)
-
-        if not success:
-            continue
-
         print("====== MAIN MENU ======")
+        print("------- (Admin) -------")
         print("[1] Create Member")
         print("[2] View Members")
         print("[3] Edit Member")
@@ -67,28 +108,53 @@ def menu(cur):
         print("[0] Log Out")
         print("=======================")
 
+        
         choice = int(input("Choice: "))
 
         match choice:
             case 1:
                 getCreateMemberInput()
-                break
             case 2: 
                 getViewMemberInput()
-                break
             case 3: 
                 getEditMemberInput()
-                break
             case 4:
                 getRemoveMemberInput()
-                break
             case 5:
                 openReportsMenu()
-                break
             case 0:
                 break
              
-                  
+def memberMenu():     
+    choice = -1
+
+    while choice != 0:
+        print("====== MAIN MENU ======")
+        print("------- (Admin) -------")
+        print("[1] Create Member")
+        print("[2] View Members")
+        print("[3] Edit Member")
+        print("[4] Remove Member")
+        print("[5] Generate Reports")
+        print("[0] Log Out")
+        print("=======================")
+
+        
+        choice = int(input("Choice: "))
+
+        match choice:
+            case 1:
+                getCreateMemberInput()
+            case 2: 
+                getViewMemberInput()
+            case 3: 
+                getEditMemberInput()
+            case 4:
+                getRemoveMemberInput()
+            case 5:
+                openReportsMenu()
+            case 0:
+                break          
 
 def main():
     print("Connecting to database")
@@ -109,8 +175,21 @@ def main():
     # Get Cursor
     cur = conn.cursor()
 
-    # Main Menu loop until program exit
-    menu(cur)
+    # Login/register prompt
+    # Should only exit when user is existing or user presses 0
+    user = auth(cur)
+
+    # Exit program when User is None
+    if user == None:
+        conn.close()
+        return
+
+    match user.accessLevel:
+        case 1:
+            memberMenu()
+        case 2:
+            adminMenu()
+
     conn.close()
 
 if __name__ == "__main__":
